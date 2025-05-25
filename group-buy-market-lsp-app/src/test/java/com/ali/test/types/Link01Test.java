a6ed5a4e2b8583832039d240dcdd288807a4cd43
aliyast <1793192224@qq.com>
2025-05-26 00:12:24 +0800

    feat:责任链模型框架设计

---
+++ group-buy-market-lsp-app/src/test/java/com/ali/test/types/Link01Test.java
@@ -0,0 +1,30 @@
+package com.ali.test.types;
+
+import com.ali.test.types.rules1.factory.Rule01TradeRuleFactory;
+import com.ali.test.types.rules2.factory.Rule02TradeRuleFactory;
+import com.ali.types.design.framework.link.model1.ILogicLink;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Link01Test {
+
+    @Resource
+    public Rule01TradeRuleFactory rule01TradeRuleFactory;
+
+    @Test
+    public void test_model01_01() throws Exception {
+        ILogicLink<String, Rule02TradeRuleFactory.DynamicContext, String> logicLink = rule01TradeRuleFactory.openLogicLink();
+        String logic = logicLink.apply("123", new Rule02TradeRuleFactory.DynamicContext());
+        log.info("测试结果:{}", JSON.toJSONString(logic));
+    }
+
+}
